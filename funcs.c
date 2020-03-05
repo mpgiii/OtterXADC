aa56d9c1c093760d8e0cd2eb7b96239e0f0ed580
mpgiii <georgariou3@gmail.com>
2020-03-05 15:51:20 -0800

    Added files

---
+++ funcs.c
@@ -0,0 +1,13 @@
+
+
+int main(int argc, char* argv[]) {
+    
+}
+
+int setStuff(int sampleRate) {
+    // The Basys 3 clock runs at 100 MHz.
+    // We need to clock divide to make the XADC run
+    // at the sample rate the user inputs.
+    
+    
+}
