30a867eabb9dfeb7ba56058c56497f05f0e350a4
mpgiii <georgariou3@gmail.com>
2020-03-05 16:47:14 -0800

    Create function to get DCLK divider

--- funcs.c
+++ funcs.c
@@ -1,13 +1,22 @@
+#include <math.h>
 
+#define conversionCycles 22
 
 int main(int argc, char* argv[]) {
     
 }
 
-int setStuff(int sampleRate) {
-    // The Basys 3 clock runs at 100 MHz.
-    // We need to clock divide to make the XADC run
-    // at the sample rate the user inputs.
+
+
+int getDclkDivider(int dclkFrequency, int acqTime, int conversionRate) {
+    // dclkFrequency inputted in MHz
+    // acqTime in clock cycles
+    // conversionRate inputted in KSPS
     
-    
+    // NOTE: multiplied by 1000 to account for dclkFrequency being in MHz.
+    return round(1000 * 
+        (dclkFrequency / (conversionRate * (acqTime + conversionCycles))));
 }
+
+// 4 cycle acquisition ( can be extended to 10 by setting ACQ bit )
+// conversion is 22 cycles
