83f9b6d11e92215a001333f17bcf9f62fb803d9a
mpgiii <georgariou3@gmail.com>
2020-03-05 16:54:51 -0800

    Working clock divider getter function

--- funcs.c
+++ funcs.c
@@ -1,12 +1,27 @@
+#include <stdio.h>
 #include <math.h>
 
 #define conversionCycles 22
 
-int main(int argc, char* argv[]) {
+int main()
+{
     
+    int dclkFreq;
+    int acqTime;
+    int conversionRate;
+    
+    printf("Enter DCLK Frequency (MHz)\n");
+    scanf("%d", &dclkFreq);
+    printf("Enter acquisition time (cycles)\n");
+    scanf("%d", &acqTime);
+    printf("Enter ADC conversion rate (KSPS)\n");
+    scanf("%d", &conversionRate);
+    
+    printf("\nYour clock divider: %d", 
+        getDclkDivider(dclkFreq, acqTime, conversionRate));
+
+    return 0;
 }
-
-
 
 int getDclkDivider(int dclkFrequency, int acqTime, int conversionRate) {
     // dclkFrequency inputted in MHz
@@ -14,8 +29,8 @@ int getDclkDivider(int dclkFrequency, int acqTime, int conversionRate) {
     // conversionRate inputted in KSPS
     
     // NOTE: multiplied by 1000 to account for dclkFrequency being in MHz.
-    return round(1000 * 
-        (dclkFrequency / (conversionRate * (acqTime + conversionCycles))));
+    return round(1000 * ((double)dclkFrequency / 
+        (conversionRate * (acqTime + conversionCycles))));
 }
 
 // 4 cycle acquisition ( can be extended to 10 by setting ACQ bit )
