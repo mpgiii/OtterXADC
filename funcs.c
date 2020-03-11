919615f79c48c81e9d8868eeb85cb53a2d97d250
mpgiii <georgariou3@gmail.com>
2020-03-10 17:03:17 -0700

    Lots of changes. See message
    - Added function to populate a sample array
    - Added default values for reg0, reg1, and reg2
    - Made dclkFreq, acqTime, conversionRate user defined
    - Made denAndDwe one bit instead of 2
    - Changed how writeDclkDivider works (no longer overwrites bottom eight bits
    - Added functions to calculate peak to peak and average values of sample array

--- funcs.c
+++ funcs.c
@@ -12,46 +12,48 @@
 
 #define DENDWEaddress 0x14000000
 
+#define EOCaddress 0x15000000
+#define sampleAddress 0x16000000
 
-int main()
-{
-    
-    int dclkFreq;
-    int acqTime;
-    int conversionRate;
-    
-    printf("Enter DCLK Frequency (MHz)\n");
-    scanf("%d", &dclkFreq);
-    printf("Enter acquisition time (cycles)\n");
-    scanf("%d", &acqTime);
-    printf("Enter ADC conversion rate (KSPS)\n");
-    scanf("%d", &conversionRate);
-    
-    printf("\nYour clock divider: %d", 
-        getDclkDivider(dclkFreq, acqTime, conversionRate));
+// DO NOT CHANGE ANYTHING ABOVE THIS LINE //
 
-    return 0;
-}
+// defines below this line can be changed //
+
+#define reg0Default 0x0003 //0000000000000011
+#define reg1Default 0x30F0 //0011000011110000
+#define reg2Default 0x0000 //0000000000000000
+
+#define dclkFreq  //in MHz
+#define acqTime 4 //in cycles. can only be 4 or 10
+#define conversionRate //in KSPS
+
 
 void setDENandDWE() {
     int* denAndDwe = DENDWEaddress;
-    *denAndDwe = 0x3;
+    *denAndDwe = 0x1;
+    // Hardware will (should) reset this value to 0.
 }
 
 /* writeDclkDivider
  * Input:  dclkDivider
+ *         reg2 (pointer to change it in main when modified)
  * Output: none */
-void writeDclkDivider(int dclkDivider) {
+void writeDclkDivider(int dclkDivider, int* reg2) {
     int* address = adcAddress;
     int* dataAddress = adcDataAddress;
     
-    //set the address we are writing to to configReg2
-    *adcAddress = configReg2;
+    int shiftedDivider = dclkDivider << 8;
+    
+    *reg2 &= 0x00FF; //clear out the old clock divider
+    *reg2 |= shiftedDivider; //and set the inputted one
+    
+    //set the address we are writing to be configReg2
+    *address = configReg2;
     
     //write the clock divider to that spot, bits shifted by 8 (other 8 unused)
-    *dataAddress = dclkDivider << 8;
+    *dataAddress = *reg2;
     
-    //set DEN and DWE high so the ADC will actually read what we wrote
+    //set DEN and DWE high so the ADC will actually read what we wrote (done in hardware)
     setDENandDWE();
 }
 
@@ -63,12 +65,82 @@ void writeDclkDivider(int dclkDivider) {
  * Output: clock divider value, rounded to nearest whole number */
 int getDclkDivider(int dclkFrequency, int acqTime, int conversionRate) {
     // dclkFrequency inputted in MHz
-    // acqTime in clock cycles
+    // acqTime in clock cycles (can only be 4 or 10. 4 by default)
     // conversionRate inputted in KSPS
     
     // NOTE: multiplied by 1000 to account for dclkFrequency being in MHz.
     return round(1000 * ((double)dclkFrequency / 
         (conversionRate * (acqTime + conversionCycles))));
+}
+
+void getSamples(int* adcSamples) {
+    int i = 0;
+    while(i < 5000) {
+        // do not collect samples until EOC goes high
+        while(!(*EOC)) {
+            ;
+        }
+        // once EOC goes high, fill up our array.
+        adcSamples[i++] = *sampleAddr;
+    }
+    return;
+}
+
+int getPeakToPeak(int* adcSamples, int N) {
+    int max = adcSamples[0];
+    int min = adcSamples[0];
+    int i;
+    for(i = 0; i < N; i++) {
+        if (adcSamples[i] < min) 
+            min = adcSamples[i];
+        if (adcSamples[i] > max)
+            max = adcSamples[i];
+    }
+    return (max - min);
+}
+
+int getAverage(int* adcSamples, int N) {
+    int sum = 0;
+    int i;
+    for(i = 0; i < N; i++) {
+        sum += adcSamples[i];
+    }
+    return (sum / N);
+}
+
+
+int main()
+{
+    
+    int reg0 = reg0Default;
+    int reg1 = reg1Default;
+    int reg2 = reg2Default;
+
+    int dclkDivider;
+    
+    int peakToPeak;
+    int average;
+    
+    int* EOC = eocAddress;
+    int* sampleAddr = sampleAddress;
+    
+    int adcSamples[5000];
+    
+    // set the clock rate from user-specified parameters, from the #defines
+    dclkDivider = getDclkDivider(dclkFreq, acqTime, conversionRate);
+    writeDclkDivider(dclkDivider, &reg2);
+    
+    // run indefinitely (until shutdown)
+    while(1){
+        // populate the samples array
+        getSamples(adcSamples);
+        // get the peak to peak of this sample
+        peakToPeak = getPeakToPeak(adcSamples, 5000);
+        // get the average of this sample
+        average = getAverage(adcSamples, 5000);
+    }
+
+    return 0;
 }
 
 // 4 cycle acquisition ( can be extended to 10 by setting ACQ bit )
